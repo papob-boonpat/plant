032b54a2765d761d7e1e34d2bbb0b12070ff70f6
papob.boonpat <boonpat.papob@gmail.com>
2021-06-19 13:12:45 +0700

    init Project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("test"), centerTitle: true,),
+      body: Center(
+        child: Text("TEST"),
+      ),
+    );
+  }
+}
